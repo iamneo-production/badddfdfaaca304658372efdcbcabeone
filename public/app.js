41c274496e3de4d5653106fc75f99627f5c096cb
Iamneo <techadmin@iamneo.ai>
2023-09-02 13:08:29 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -26,13 +26,13 @@ const makeMove = (indexX, indexY) => {
 
         // Check for a win after each move
         if (checkWin()) {
-            result.textContent = `Player ${currentPlayer} wins`;
+            result.textContent = `Player ${currentPlayer} won`;
             disableAllButtons();
         } else if (cells.every((cell) => cell !== '')) {
             result.textContent = "It's a draw";
         } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            result.textContent = `Player ${currentPlayer}'s Turn`;
+            result.textContent = `Player ${currentPlayer} Turn`;
         }
     }
 };
