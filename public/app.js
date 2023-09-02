e7dd303ab4a9fd950410c3fcc466ddf094f1774c
Iamneo <techadmin@iamneo.ai>
2023-09-02 12:39:18 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
-// Constants
-const SIZE = 3; // Size of the grid
-const WINNING_CONDITIONS = [
+// Initial game state
+let cells = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('.btn');
+let conditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -11,33 +14,15 @@ const WINNING_CONDITIONS = [
     [2, 4, 6]
 ];
 
-// Initial game state
-let cells = Array(SIZE * SIZE).fill('');
-let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let box = document.querySelector('.box');
-let resetButton = document.querySelector('#reset');
-
-// Create the grid dynamically
-for (let i = 0; i < SIZE; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < SIZE; j++) {
-        const index = i * SIZE + j;
-        const btn = document.createElement('button');
-        btn.classList.add('btn');
-        btn.dataset.index = index;
-        row.appendChild(btn);
-    }
-    box.appendChild(row);
-}
-
 // Function to handle player moves
-const makeMove = (index) => {
+const makeMove = (indexX, indexY) => {
+    const index = indexX * 3 + indexY;
+    const element = btns[index];
+    
     if (cells[index] === '' && !checkWin()) {
         cells[index] = currentPlayer;
-        btns[index].textContent = currentPlayer;
-        btns[index].disabled = true;
+        element.textContent = currentPlayer;
+        element.disabled = true;
 
         // Check for a win after each move
         if (checkWin()) {
@@ -54,7 +39,7 @@ const makeMove = (index) => {
 
 // Function to check for a win
 const checkWin = () => {
-    for (const condition of WINNING_CONDITIONS) {
+    for (const condition of conditions) {
         const [a, b, c] = condition;
         if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
             return true;
@@ -72,27 +57,25 @@ const disableAllButtons = () => {
 
 // Function to reset the game
 const resetGame = () => {
-    cells = Array(SIZE * SIZE).fill('');
+    cells = ['', '', '', '', '', '', '', '', ''];
     currentPlayer = 'X';
-    result.textContent = `Player ${currentPlayer}'s Turn`;
 
     btns.forEach((btn, i) => {
         btn.textContent = '';
         btn.disabled = false;
-        btn.addEventListener('click', () => makeMove(i));
     });
+
+    // Reset the result message
+    result.textContent = `Player ${currentPlayer}'s Turn`;
 };
 
 // Event delegation to handle button clicks
-box.addEventListener('click', (event) => {
+document.querySelector('.box').addEventListener('click', (event) => {
     const button = event.target;
     if (button.classList.contains('btn')) {
         const index = button.getAttribute('data-index');
-        makeMove(index);
+        makeMove(Math.floor(index / 3), index % 3);
     }
 });
 
-resetButton.addEventListener('click', resetGame);
-
-// Store references to buttons for enabling/disabling
-let btns = document.querySelectorAll('.btn');
+document.querySelector('#reset').addEventListener('click', resetGame);
